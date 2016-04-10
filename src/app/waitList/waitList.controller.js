125e7133d36b5f986c970fdd68855e668b54f00c
jmtoung@gmail.com <jmtoung@gmail.com>
2016-04-10 15:16:43 -0700

    Using ng-click

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -9,6 +9,12 @@
         var vm = this;
         
         vm.parties = [1, 2, 3, 4];
+        
+        vm.addParty = addParty;
+        
+        function addParty() {
+            vm.parties.push('another');
+        }
     }
     
 })();
