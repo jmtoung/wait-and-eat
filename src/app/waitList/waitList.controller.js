26b095f32baa82943258be9d49f55f29e93c8751
jmtoung@gmail.com <jmtoung@gmail.com>
2016-04-10 15:50:01 -0700

    Using ng-model and creating new Party objects

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -10,13 +10,23 @@
     function WaitListController($firebaseArray) {
         var vm = this;
         
-        var fireParties = new Firebase('https://waitandeat-jmt.firebaseio.com/parties');
+        var fireParties = new Firebase('https://waitandeat-jmt.firebaseio.com/partiesU');
+        
+        function Party() {
+            this.name = '';
+            this.phone = '';
+            this.size = '';
+            this.done = false;
+            this.notified = false;
+        }
+        
+        vm.newParty = new Party();
         vm.parties = $firebaseArray(fireParties);
         
         vm.addParty = addParty;
         
         function addParty() {
-            vm.parties.$add('another');
+            vm.parties.$add(vm.newParty);
         }
     }
     
