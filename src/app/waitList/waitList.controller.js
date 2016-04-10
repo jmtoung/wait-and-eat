8b45bd26a872e28bcf3d29c3a61c35ca6e9f5f56
jmtoung@gmail.com <jmtoung@gmail.com>
2016-04-10 16:38:33 -0700

    added code for removing parties

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -22,12 +22,16 @@
         
         vm.newParty = new Party();
         vm.parties = $firebaseArray(fireParties);
-        
         vm.addParty = addParty;
+        vm.removeParty = removeParty;
         
         function addParty() {
             vm.parties.$add(vm.newParty);
             vm.newParty = new Party();
+        }
+        
+        function removeParty(party) {
+            vm.parties.$remove(party);
         }
     }
     
