8c8acaf03bfe1d775586bb3bc8f9c70f01c44707
jmtoung@gmail.com <jmtoung@gmail.com>
2016-04-10 22:20:02 -0700

    Added code for promises on AuthController.register

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -5,6 +5,29 @@
         .module('app.auth')
         .controller('AuthController', AuthController);
 
-    function AuthController() {}
+    AuthController.$inject = ['$firebaseAuth'];
+    
+    function AuthController($firebaseAuth) {
+        var vm = this;
+        var firebaseReference = new Firebase('https://waitandeat-jmt.firebaseio.com/');
+        var firebaseAuthObject = $firebaseAuth(firebaseReference);
+        
+        vm.user = {
+            email: '',
+            password: ''
+        }
+        vm.register = register;
+        
+        function register(user) {
+            return firebaseAuthObject.$createUser(user)
+                .then(function(user) {
+                    console.log(user);
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+            
+        }
+    }
 
 })();
