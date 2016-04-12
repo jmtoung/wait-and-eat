3cfb81357387d8ee858844ee092fcbe4811f3930
jmtoung@gmail.com <jmtoung@gmail.com>
2016-04-11 20:38:37 -0700

    Initial structure for party.service.js

---
+++ src/app/core/party.service.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+    
+    angular
+        .module('app.core')
+        .factory('partySerivce', partyService);
+    
+    function partyService() {
+    
+        var service = {}
+        
+        return service;
+    }
+    
+})();
