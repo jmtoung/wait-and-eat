130328230d7e9e0b3ae37549949b2d332d09b6fc
jmtoung@gmail.com <jmtoung@gmail.com>
2016-04-12 19:57:37 -0700

    Added Party constructor to party.service.js

--- src/app/core/party.service.js
+++ src/app/core/party.service.js
@@ -7,9 +7,19 @@
     
     function partyService() {
     
-        var service = {}
+        var service = {
+            Party: Party
+        };
         
         return service;
+        
+        function Party() {
+            this.name = '';
+            this.phone = '';
+            this.size = '';
+            this.done = false;
+            this.notified = false;
+        }
     }
     
 })();
