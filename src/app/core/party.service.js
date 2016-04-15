4822ed6e401e1d564b28a581471e04b7a92c68a5
jmtoung@gmail.com <jmtoung@gmail.com>
2016-04-14 20:20:22 -0700

    partyService now gets parties by user.

--- src/app/core/party.service.js
+++ src/app/core/party.service.js
@@ -11,7 +11,7 @@
     
         var service = {
             Party: Party,
-            parties: $firebaseArray(firebaseDataService.root.child('parties'))
+            getPartiesByUser
         };
         
         return service;
@@ -23,6 +23,10 @@
             this.done = false;
             this.notified = false;
         }
+    }
+    
+    function getPartiesByUser(uid) {
+        return $firebaseArray(firebaseDataService.users.child(uid).child('parties'));    
     }
     
 })();
